4f24f673a2308bc24d2fee8d5eca7c979376980a
Eudis Alves <deleudis@gmail.com>
2023-07-27 18:26:15 -0300

    adicionando novas pages

--- controle-mercadoria/main.js
+++ controle-mercadoria/main.js
@@ -1,31 +1,27 @@
-const conatiner = pageNumber = document.getElementById('conatiner');
-const selecyors = document.querySelectorAll('.selector');
+const container = document.getElementById('container');
+const selectors = document.querySelectorAll('.selector');
 let currentPage = 0;
 
-function scrollTopage(pageNumber) {
-
-    const offset = pageNumber * window.innerWidth;
-    conatiner.style.transform = `translateX(-${offset}px)`;
-    currentPage = pageNumber;
-    updateSelectors();
-
+function scrollToPage(pageNumber) {
+  const offset = pageNumber * window.innerWidth;
+  container.style.transform = `translateX(-${offset}px)`;
+  currentPage = pageNumber;
+  updateSelectors();
 }
 
 function updateSelectors() {
-    updateSelectors.forEach((selecttor, index) => {
-        selecttor.style.backgroundColor = currentPage === index ? '#ccc' : '#eee';
-    });
+  selectors.forEach((selector, index) => {
+    selector.style.backgroundColor = currentPage === index ? '#ccc' : '#eee';
+  });
 }
 
-// eventos de rolagem com setas
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowLeft') {
+    currentPage = Math.max(0, currentPage - 1);
+  } else if (event.key === 'ArrowRight') {
+    currentPage = Math.min(13, currentPage + 1);
+  }
+  scrollToPage(currentPage);
+});
 
-document.addEventListener('keydown', (event) =>{
-    if (event.key === 'ArrowLeft') {
-        currentPage = Math.max(0, currentPage-1);
-    } else if (EventSubscriptionVendor.key === 'ArrowRight') {
-        currentPage = Math.min(6, currentpage +1);
-    }
-    scrollTopage(currentPage);
-} );
-
-updateSelectors(); //inicialmente, definir a cor do seletor para a página atual
+updateSelectors(); // Inicialmente, definir a cor do seletor para a página atual
